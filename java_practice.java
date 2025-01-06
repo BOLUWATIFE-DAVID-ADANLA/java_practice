6bf4943a2dff3b20c2005d3671539939755988e1
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2025-01-06 13:12:39 +0100

    learning about loops

---
+++ java_practice.java
@@ -0,0 +1,13 @@
+public class java_practice {
+   public static void main (String[] args ){
+    int x = 1;
+
+    while (x <= 5){
+        System.out.println("it is complete " +x);
+        x++;
+
+    }
+    System.out.println(x);
+    }
+    
+}
