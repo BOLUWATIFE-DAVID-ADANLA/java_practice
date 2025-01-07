aa6c78db93a6a80a3b176fbf9d6d2e264b1945c4
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2025-01-07 11:11:34 +0100

    for loop exercise

--- java_practice.java
+++ java_practice.java
@@ -20,6 +20,18 @@ public class java_practice {
     //     System.out.println("we print " + i);
     // }
 
+//     int i, j;
+
+// i = 100;
+
+// j = 300;
+
+// while(i++ < j--);
+
+// System.out.println(i);
+
+
+
     }
     
 }
