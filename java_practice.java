fb636f8033d3e501a83ec8b8334f1e2f3941a491
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2025-01-07 10:36:11 +0100

    learnt about while and do while loop

--- java_practice.java
+++ java_practice.java
@@ -1,13 +1,21 @@
 public class java_practice {
    public static void main (String[] args ){
-    int x = 1;
+    // int x = 1;
+    // while (x <= 5){
+    //     System.out.println("it is complete " +x);
+    //     x++;
+    // }
+    // System.out.println(x);
 
-    while (x <= 5){
-        System.out.println("it is complete " +x);
+    //  do while loop  a do while loop would execute at least once even if the condition is false 
+
+     int x = 1;
+     do{
+        System.out.println("it is " + x);
         x++;
+     }while(x <= 4);
 
-    }
-    System.out.println(x);
+
     }
     
 }
